5ac22bca076b665f76d9873271f0d8e8f35c5dd7
KyjaDevs <victor.kyja.devs@gmail.com>
2022-08-30 23:29:14 -0500

    init commit

---
+++ exodus-api/src/main/java/com/legion/exodusapi/ExodusApiApplication.java
@@ -0,0 +1,13 @@
+package com.legion.exodusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExodusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExodusApiApplication.class, args);
+	}
+
+}
---
+++ exodus-api/src/test/java/com/legion/exodusapi/ExodusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legion.exodusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExodusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
